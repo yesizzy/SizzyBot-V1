9a4975596640da8f7f91079d77328671f4f37aa2
sizzy <pmoin0457@gmail.com>
2026-01-21 11:05:53 +0000

    fixed it so that the bot can now join party's

--- main.py
+++ main.py
@@ -8,20 +8,18 @@ import sys
 from pathlib import Path
 
 configFile = "config.json"
-Api = "https://fortnite-api.com/v2/cosmetics/br/search/all"
+API = "https://fortnite-api.com/v2/cosmetics/br/search/all"
 
-cosmeticMap = {
-    'outfit': {'backendType': 'AthenaCharacter', 'methodName': 'set_outfit'},
-    'emote': {'backendType': 'AthenaDance', 'methodName': 'set_emote'},
-    'backpack': {'backendType': 'AthenaBackpack', 'methodName': 'set_backpack'},
-    'pickaxe': {'backendType': 'AthenaPickaxe', 'methodName': 'set_pickaxe'},
-    'sidekick': {'backendType': 'AthenaPet', 'methodName': 'set_pet'},
-    'shoes': {'backendType': 'AthenaShoes', 'methodName': 'set_shoes'},
-    'glider': {'backendType': 'AthenaGlider', 'methodName': 'set_glider'},
-    'contrail': {'backendType': 'AthenaContrail', 'methodName': 'set_contrail'}
+Type = {
+    'outfit': {'backendType': 'AthenaCharacter', 'name': 'outfit'},
+    'emote': {'backendType': 'AthenaDance', 'name': 'emote'},
+    'backpack': {'backendType': 'AthenaBackpack', 'name': 'backpack'},
+    'pickaxe': {'backendType': 'AthenaPickaxe', 'name': 'pickaxe'},
+    'glider': {'backendType': 'AthenaGlider', 'name': 'glider'},
+    'contrail': {'backendType': 'AthenaContrail', 'name': 'contrail'}
 }
 
-def grabConfig():
+def Config():
     if not Path(configFile).exists():
         sys.exit(1)
     
@@ -45,18 +43,18 @@ def grabConfig():
         'addUsers': fortnite.get('party', {}).get('add_users', True)
     }
 
-async def findCosmeticId(itemName, cosmeticType):
-    if cosmeticType not in cosmeticMap:
+async def Cosmetics(itemName, cosmeticType):
+    if cosmeticType not in Type:
         return None
     
     searchParams = {
         'name': itemName,
-        'backendType': cosmeticMap[cosmeticType]['backendType']
+        'backendType': Type[cosmeticType]['backendType']
     }
     
     async with aiohttp.ClientSession() as session:
         try:
-            async with session.get(Api, params=searchParams) as resp:
+            async with session.get(API, params=searchParams) as resp:
                 if resp.status == 200:
                     apiData = await resp.json()
                     if apiData['data']:
@@ -65,7 +63,7 @@ async def findCosmeticId(itemName, cosmeticType):
             pass
     return None
 
-def setupBot(config):
+def setup(config):
     if config['device'] and config['secretKey'] and config['accountId']:
         authMethod = rebootpy.DeviceAuth(
             device_id=config['device'],
@@ -83,49 +81,43 @@ def setupBot(config):
     
     return botInstance
 
-def wipeConsole():
+def console():
     if platform.system() == 'Windows':
         os.system('cls')
     else:
         os.system('clear')
 
-async def changeCosmetic(bot, cosmeticType, itemName):
-    cosmeticId = await findCosmeticId(itemName, cosmeticType)
+async def cosmetics(bot, cosmeticType, itemName):
+    cosmeticId = await Cosmetics(itemName, cosmeticType)
     
     if cosmeticId and bot.party:
-        methodToCall = cosmeticMap[cosmeticType]['methodName']
+        methodToCall = Type[cosmeticType]['name']
         cosmeticMethod = getattr(bot.party.me, methodToCall)
         await cosmeticMethod(cosmeticId)
-    
-async def loadDefaults(bot, config):
+
+async def load(bot, config):
     if not bot.party:
         return
     
-    defaultItems = config['Cosmetic']  
+    Items = config['Cosmetic']  
     
-    if defaultItems.get('cid'):
-        await bot.party.me.set_outfit(defaultItems['cid'])
+    if Items.get('cid'):
+        await bot.party.me.outfit(Items['cid'])
     
-    if defaultItems.get('eid'):
-        await bot.party.me.set_emote(defaultItems['eid'])
+    if Items.get('eid'):
+        await bot.party.me.emote(Items['eid'])
     
-    if defaultItems.get('backpack'):
-        await bot.party.me.set_backpack(defaultItems['backpack'])
+    if Items.get('backpack'):
+        await bot.party.me.backpack(Items['backpack'])
     
-    if defaultItems.get('pickaxe'):
-        await bot.party.me.set_pickaxe(defaultItems['pickaxe'])
+    if Items.get('pickaxe'):
+        await bot.party.me.pickaxe(Items['pickaxe'])
     
-    if defaultItems.get('sidekick'):
-        await bot.party.me.set_pet(defaultItems['sidekick'])
+    if Items.get('glider'):
+        await bot.party.me.glider(Items['glider'])
     
-    if defaultItems.get('shoes'):
-        await bot.party.me.set_shoes(defaultItems['shoes'])
-    
-    if defaultItems.get('glider'):
-        await bot.party.me.set_glider(defaultItems['glider'])
-    
-    if defaultItems.get('contrail'):
-        await bot.party.me.set_contrail(defaultItems['contrail'])
+    if Items.get('contrail'):
+        await bot.party.me.contrail(Items['contrail'])
     
     bannerSettings = config['banner']
     if bannerSettings.get('icon'):
@@ -134,22 +126,19 @@ async def loadDefaults(bot, config):
             color=bannerSettings.get('color', 'DefaultColor')
         )
 
-async def joinUserParty(bot, username):
+async def join(bot, username):
     try:
         userProfile = await bot.fetch_user(user=username, cache=True)
         if not userProfile:
             return
         
-        friendConnection = bot.get_friend(userProfile.id)
-        if friendConnection:
-            await friendConnection.join_party()
+        connections = bot.get_friend(userProfile.id)
+        if connections:
+            await connections.join_party()
     except:
         pass
 
-def showCommands():
-    pass
-
-async def handleInput(bot, config):
+async def Input(bot, config):
     while bot.party:
         try:
             userInput = input("\nSizzyBot > ").strip()
@@ -167,29 +156,23 @@ async def handleInput(bot, config):
             arguments = splitInput[1:]
             
             if command in ['clear', 'cls']:
-                wipeConsole()
+                console()
             elif command == 'outfit':
-                await changeCosmetic(bot, 'outfit', ' '.join(arguments))
+                await cosmetics(bot, 'outfit', ' '.join(arguments))
             elif command == 'emote':
-                await changeCosmetic(bot, 'emote', ' '.join(arguments))
+                await cosmetics(bot, 'emote', ' '.join(arguments))
             elif command == 'backpack':
-                await changeCosmetic(bot, 'backpack', ' '.join(arguments))
+                await cosmetics(bot, 'backpack', ' '.join(arguments))
             elif command == 'pickaxe':
-                await changeCosmetic(bot, 'pickaxe', ' '.join(arguments))
-            elif command == 'sidekick':
-                await changeCosmetic(bot, 'sidekick', ' '.join(arguments))
-            elif command == 'shoes':
-                await changeCosmetic(bot, 'shoes', ' '.join(arguments))
+                await cosmetics(bot, 'pickaxe', ' '.join(arguments))
             elif command == 'glider':
-                await changeCosmetic(bot, 'glider', ' '.join(arguments))
+                await cosmetics(bot, 'glider', ' '.join(arguments))
             elif command == 'contrail':
-                await changeCosmetic(bot, 'contrail', ' '.join(arguments))
+                await cosmetics(bot, 'contrail', ' '.join(arguments))
             elif command == 'join':
-                await joinUserParty(bot, ' '.join(arguments))
+                await join(bot, ' '.join(arguments))
             elif command == 'leave':
                 await bot.party.me.leave()
-            elif command == 'help':
-                showCommands()
             elif command == 'exit':
                 sys.exit()
                 
@@ -198,17 +181,17 @@ async def handleInput(bot, config):
         except:
             pass
 
-async def onBotReady(bot, config):
-    await loadDefaults(bot, config)
-    await handleInput(bot, config)
+async def Ready(bot, config):
+    await load(bot, config)
+    await Input(bot, config)
 
-def launchBot():
-    config = grabConfig()
-    bot = setupBot(config)
+def launch():
+    config = Config()
+    bot = setup(config)
     
     @bot.event
     async def event_ready():
-        await onBotReady(bot, config)
+        await Ready(bot, config)
     
     try:
         bot.run()
@@ -216,4 +199,4 @@ def launchBot():
         sys.exit(1)
 
 if __name__ == "__main__":
-    launchBot()
+    launch()
